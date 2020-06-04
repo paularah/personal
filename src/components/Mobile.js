0501229b5c27e27a7a279db1a1b96e01798e6c78
paularah <p.arah@alustudent.com>
2020-06-04 23:09:02 +0100

    add mobile view

--- src/components/Mobile.js
+++ src/components/Mobile.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import HomepageHeading from './Heading';
 
 import {
-    Menu,
     Segment,
     Responsive,
+    Sidebar,
 
 } from 'semantic-ui-react';
 
@@ -31,23 +31,6 @@ class MobileContainer extends Component {
           getWidth={getWidth}
           maxWidth={Responsive.onlyMobile.maxWidth}
         >
-          <Sidebar
-            as={Menu}
-            animation='push'
-            inverted
-            onHide={this.handleSidebarHide}
-            vertical
-            visible={sidebarOpened}
-          >
-            <Menu.Item as='a' active>
-              Home
-            </Menu.Item>
-            <Menu.Item as='a'>Work</Menu.Item>
-            <Menu.Item as='a'>Company</Menu.Item>
-            <Menu.Item as='a'>Careers</Menu.Item>
-            <Menu.Item as='a'>Log in</Menu.Item>
-            <Menu.Item as='a'>Sign Up</Menu.Item>
-          </Sidebar>
   
           <Sidebar.Pusher dimmed={sidebarOpened}>
             <Segment
@@ -56,21 +39,6 @@ class MobileContainer extends Component {
               style={{ minHeight: 350, padding: '1em 0em' }}
               vertical
             >
-              <Container>
-                <Menu inverted pointing secondary size='large'>
-                  <Menu.Item onClick={this.handleToggle}>
-                    <Icon name='sidebar' />
-                  </Menu.Item>
-                  <Menu.Item position='right'>
-                    <Button as='a' inverted>
-                      Log in
-                    </Button>
-                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
-                      Sign Up
-                    </Button>
-                  </Menu.Item>
-                </Menu>
-              </Container>
               <HomepageHeading mobile />
             </Segment>
   
